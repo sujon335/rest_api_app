839fbc64a53828f3905eed0131b3dff0457dcdbd
rakib_asif <md.rakib.amin@gmail.com>
2015-06-14 00:17:13 -0800

    api

--- application/controllers/Rest_app.php
+++ application/controllers/Rest_app.php
@@ -1,33 +1,29 @@
 <?php
-require(APPPATH.'/libraries/REST_Controller.php');
 
-class Rest_app extends REST_Controller
-{
-    function index()
-    {
-        $this->response(array('name'=>'magi'));
+require(APPPATH . '/libraries/REST_Controller.php');
+
+class Rest_app extends REST_Controller {
+
+    function index() {
+        $this->response(array('name' => 'magi'));
     }
-    function user()
-    {
-        if(!$this->get('id'))
-        {
+
+    function user() {
+        if (!$this->get('id')) {
             $this->response(NULL, 400);
         }
 
-        $this->db->where('employee_id',$this->get('id'));
-        $query=$this->db->get('employees');
-        $user = $query->result();
-        if($user)
-        {
-            $this->response($user, 200); // 200 being the HTTP response code
+        $this->db->where('employee_id', $this->get('id'));
+        $q = $this->db->get('employees');
+        $data = array();
+        if ($q->num_rows()) {
+            $data['status'] = 'success';
+            $data['data'] = $q->result();
+        } else {
+            $data['status'] = 'failed';
         }
-
-        else
-        {
-            $this->response(NULL, 404);
-        }
+        $this->response($data);
     }
 
 }
 
-?>
