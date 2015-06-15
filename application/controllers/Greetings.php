41957a986fc6deb3ecae43e051c9ae9ceff2a7d0
rakib_asif <md.rakib.amin@gmail.com>
2015-06-15 13:29:36 -0800

    gret

---
+++ application/controllers/Greetings.php
@@ -0,0 +1,24 @@
+<?php
+
+require(APPPATH . '/libraries/REST_Controller.php');
+
+class Rest_app extends REST_Controller {
+
+    function answer_get() {
+        $data = array();
+        if (!$this->get('q')) {
+            $this->response(NULL, 400);
+        }
+
+        $q = urldecode($this->get('q'));
+
+        if ((strpos($q, 'Hi!') !== false) || (strpos($q, 'Hello!') !== false) || (strpos($q, 'Good morning!') !== false)||(strpos($q, 'Good evening!') !== false)||(strpos($q, 'Good night!') !== false)) {
+
+            $data['status'] = 'Hello, Kitty! I am busy with screening test';
+        } else {
+            $data['status'] = 'Invalid Question';
+        }
+        $this->response($data);
+    }
+
+}
