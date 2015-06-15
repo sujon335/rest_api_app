55573cc5bf4ccc545f713d781b8a0ea64c9ab55f
rakib_asif <md.rakib.amin@gmail.com>
2015-06-15 14:41:23 -0800

    j

--- application/controllers/weather.php
+++ application/controllers/weather.php
@@ -3,21 +3,25 @@
 class Weather extends CI_Controller {
 
     function index() {
-        if (($_GET['q'])) {
 
-            $data = array();
-            $q = $_GET['q'];
-            $city_name='chittagong';
+        echo "hello";
+//        if (($_GET['q'])) {
+//
+//            $data = array();
+//            $q = $_GET['q'];
+//            $city_name='chittagong';
+//
+//                $weather = json_decode(
+//    file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=$city_name")
+//);
+//            $data['answer']=$weather->weather;
+//            $result = json_encode($data);
+//            echo $result;
+//        } else {
+//            echo show_404();
+//        }
 
-                $data['answer'] = json_decode(
-    file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=$city_name")
-);
 
-            $result = json_encode($data);
-            echo $result;
-        } else {
-            echo show_404();
-        }
     }
 
 }
