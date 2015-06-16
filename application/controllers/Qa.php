4fe78f40710ea85ab8a99b24b0908f77d2060630
rakib_asif <md.rakib.amin@gmail.com>
2015-06-16 00:36:48 -0800

    3rd one completed

--- application/controllers/Qa.php
+++ application/controllers/Qa.php
@@ -3,11 +3,39 @@
 class Qa extends CI_Controller {
 
     function index() {
-            $data=array();
-            $data['answer']= 'Your majesty Jon Snow knows nothing So do I';
+        if (isset($_GET['q'])) {
 
-            $result = json_encode($data);
-            echo $result;
+            $data = array();
+            $q = $_GET['q'];
+            $question = urlencode($q);
+            $data = json_decode(file_get_contents("http://quepy.machinalis.com/engine/get_query?question=$question"));
+
+            $sparqlQuery = $data->queries[0]->query;
+
+            $sparqlQuery = substr($sparqlQuery, strpos($sparqlQuery, 'SELECT'), strpos($sparqlQuery, '}'));
+
+
+
+            $sparqlQuery = urlencode($sparqlQuery);
+
+            $data = json_decode(file_get_contents("http://dbpedia.org/sparql?query=$sparqlQuery&format=json"));
+
+            $xxx = $data->head->vars[0];
+            $array = $data->results->bindings;
+
+            $a = "xml:lang";
+
+
+            for ($i = 0; $i < sizeof($array); $i++) {
+                $temp = $array["$i"]->$xxx->$a;
+                if ($temp == "en")
+                    echo $array["$i"]->$xxx->value;
+                echo "  ";
+            }
+        }
+        else {
+            echo show_404();
+        }
     }
-    
+
 }
